c25acbf3b347cccbe38e36969762efcde21b4cb8
c.quinn <c.quinn@outlook.com>
2013-10-20 18:23:30 +0100

    Everything

---
+++ Life-of-Stevie-Wonder/Scripts/site.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+
+    $('.rollover .cell2').hover(function () {
+        var img = $(this).children('img');
+        img.hide();
+        img.siblings('.imageText').show();
+
+
+
+
+    }, function () {
+
+        var img = $(this).children('.imageText');
+        img.hide();
+        img.siblings('img').show();
+
+    });
+
+    $("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+
+    $("a").click(function (event) {
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);
+    });
+
+    function redirectPage() {
+        window.location = linkLocation;
+    }
+
+    if ($('audio').length > 0) {
+        $('audio')[0].play();
+    }
+
+
+    $('video').click(function () {
+        if ($('audio').length > 0) {
+            $('audio')[0].pause();
+        }
+
+    });
+
+    $('audio').click(function () {
+        if ($('video').length > 0) {
+            $('video')[0].pause();
+        }
+
+    });
+
+});
